562256252e07554137fe5b88810b8dcbd9a63090
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-06-15 12:32:49 +0300

    core[controllers[docs_to_chunks]]newline

--- src/controllers/docs_to_chunks.py
+++ src/controllers/docs_to_chunks.py
@@ -135,3 +135,4 @@ def load_and_chunk(file_path: Optional[str] = None) -> Dict[str, Any]:
     except (AttributeError, KeyError) as e:
         logger.error(DocToChunksMsg.OUTPUT_ERROR.value.format(e))
         return {}
+
