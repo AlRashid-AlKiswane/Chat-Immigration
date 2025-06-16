7e4fad185ec603fd5d5f2f10fe186c2fce3e94d8
Rama-Amairy <ramaamayri@gmail.com>
2025-06-16 15:42:57 +0300

    core[routes] replace the msg with enums msg - finish

--- src/enums/__init__.py
+++ src/enums/__init__.py
@@ -3,5 +3,5 @@
 from .monitoring_enums import MonitoringLogMsg
 from .file_preprocessing_enums import FilePreprocessingMsg
 from .docs_to_chunks_enums import DocToChunksMsg
-from .routes_enums import FileUploadMsg
+from .routes_enums import FileUploadMsg, DocsToChunks
 from .table_db_enums import ClearMsg, EngineMsg, InsertMsg, QueryMsg, TablesMsg
