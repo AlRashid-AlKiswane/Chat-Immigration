c4d8736e2a025e10ce4a73c08970248fe7e71366
AlRashid-Al-Kiswane <rashidissa2001@icloud.com>
2025-06-16 09:21:03 +0300

    feat(enums): change the enums stracture

--- src/enums/__init__.py
+++ src/enums/__init__.py
@@ -3,4 +3,6 @@
 from .monitoring_enums import MonitoringLogMsg
 from .file_preprocessing_enums import FilePreprocessingMsg
 from .docs_to_chunks_enums import DocToChunksMsg
-from .route_upload_docs_enums import FileUploadMsg
+from .routes.route_upload_docs_enums import FileUploadMsg
+from .embeddings.enums_api_model import EmbeddingLogMessages
+from .embeddings.enums_local_model import SentenceEmbeddingLogMessages
