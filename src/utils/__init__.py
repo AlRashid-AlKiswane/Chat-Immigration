ebc92b5b69a8df554c8f7ac92909b6c17cfddc1d
Al-Rashid <alrashidissa2001@icloud.com>
2025-08-01 00:46:12 +0300

    write chunk page html & authntication

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,8 +4,4 @@
 
 from .prepare_chunks import prepare_chunks_for_insertion
 from .load_json import load_json_file
-<<<<<<< HEAD
-=======
-from .get_user_auth import get_current_superuser, get_current_user
 from .form_input_preprocessing import transform_assessment_to_crs_params,create_crs_response_data
->>>>>>> 421867d2cfb1f4d017dd8ac944502c5b3fd27b51
